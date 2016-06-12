62b0312ea491c65163591f65fec19371022b222a
Nathaniel J. Smith <njs@pobox.com>
2016-06-11 18:49:42 -0700

    Delete unused code

--- redll/destruct.py
+++ redll/destruct.py
@@ -9,17 +9,10 @@ from collections.abc import MutableMapping
 #
 # Now you can do:
 #
-#   h = COFF_HEADER.read_from(file_handle)
-#   h["magic"]
-#   h["magic"] = whatever
-#   h.write_to(file_handle)
-#
-# or if you have a mutable buffer (bytesarray, memoryview, ...):
-#
-#   h = COFF_HEADER.unpack_from(buf, offset)
-#   h["magic"]
-#   h["magic"] = whatever
-#   h.write_back()  # modifies original buffer
+#   header = COFF_HEADER.view(buf, offset)
+#   header["magic"]
+#   # Mutates 'buf' in-place
+#   header["magic"] = whatever
 class StructType(object):
     def __init__(self, name, fields, endian="<"):
         self._name = name
@@ -30,16 +23,9 @@ class StructType(object):
         self._struct_string = endian + "".join(self._types)
         self.size = struct.calcsize(self._struct_string)
 
-    def _values_to_struct(self, values):
-        return Struct(self, dict(zip(self._names, values)))
-
-    def unpack(self, data):
-        values = struct.unpack(self._struct_string, data)
-        return self._values_to_struct(values)
-
     def _unpack_from(self, buf, offset):
         values = struct.unpack_from(self._struct_string, buf, offset)
-        return self._values_to_struct(values)
+        return dict(zip(self._names, values))
 
     def view(self, buf, offset):
         return StructView(self, buf, offset)
@@ -49,40 +35,10 @@ class StructType(object):
         assert len(values) == len(value_dict)
         return values
 
-    def pack(self, value_dict):
-        return struct.pack(self._struct_string,
-                           *self._values_from_dict(value_dict))
-
     def pack_into(self, buf, offset, value_dict):
         return struct.pack_into(self._struct_string,
                                 buf, offset,
                                 *self._values_from_dict(value_dict))
-
-    def read_from(self, f):
-        data = f.read(self.size)
-        return self.unpack(data)
-
-class Struct(dict):
-    def __init__(self, struct_type, values):
-        self._struct_type = struct_type
-        self.update(values)
-        assert set(self) == set(self._struct_type._names)
-
-    def __repr__(self):
-        return "Struct({}, {})".format(
-            self._struct_type._name,
-            ", ".join("{}={}".format(n, repr(self._values[n]))
-                      for n in self._struct_type._names))
-
-    def pack(self):
-        return self._struct_type.pack(self)
-
-    def write_to(self, f):
-        return f.write(self.pack())
-
-    @property
-    def size(self):
-        return self._struct_type.size
 
 def _repr_field(struct_code, value):
     if struct_code in "bBhHiIlLqQnN":
