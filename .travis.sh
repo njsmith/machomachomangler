057b90622de89a4f4c3ac6565afb75ccb3e7cb1d
Nathaniel J. Smith <njs@pobox.com>
2016-12-06 19:12:34 -0800

    [travis] more osx

--- .travis.sh
+++ .travis.sh
@@ -6,7 +6,11 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     curl -o macpython.pkg https://www.python.org/ftp/python/${MACPYTHON}/python-${MACPYTHON}-macosx10.6.pkg
     sudo installer -pkg macpython.pkg -target /
     ls /Library/Frameworks/Python.framework/Versions/*/bin/
-    PYTHON_EXE=/Library/Frameworks/Python.framework/Versions/*/bin/python
+    # XX will have to update this if we start supporting py2
+    PYTHON_EXE=/Library/Frameworks/Python.framework/Versions/*/bin/python3
+    sudo $PYTHON_EXE -m pip install virtualenv
+    sudo $PYTHON_EXE -m virtualenv testenv
+    source testenv/bin/activate
 fi
 
 pip install -U pip
