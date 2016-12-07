06b1afad52797cd5c356d9c3e4a3defc653523af
Nathaniel J. Smith <njs@pobox.com>
2016-12-06 21:07:07 -0800

    [travis] osx expr is not like GNU expr

--- .travis.sh
+++ .travis.sh
@@ -6,10 +6,10 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     curl -o macpython.pkg https://www.python.org/ftp/python/${MACPYTHON}/python-${MACPYTHON}-macosx10.6.pkg
     sudo installer -pkg macpython.pkg -target /
     ls /Library/Frameworks/Python.framework/Versions/*/bin/
-    if expr match "${MACPYTHON}" "3.*"; then
-        PYBASE=python3
-    else
+    if expr "${MACPYTHON}" : 2; then
         PYBASE=python
+    else
+        PYBASE=python3
     fi
     PYTHON_EXE=/Library/Frameworks/Python.framework/Versions/*/bin/${PYBASE}
     sudo $PYTHON_EXE -m pip install virtualenv
