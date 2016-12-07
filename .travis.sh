e732ccffe8837a13ab0c0408775d8fa39a7472ef
Nathaniel J. Smith <njs@pobox.com>
2016-12-06 20:58:55 -0800

    [travis] Attempt to proof against py2

--- .travis.sh
+++ .travis.sh
@@ -6,8 +6,12 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     curl -o macpython.pkg https://www.python.org/ftp/python/${MACPYTHON}/python-${MACPYTHON}-macosx10.6.pkg
     sudo installer -pkg macpython.pkg -target /
     ls /Library/Frameworks/Python.framework/Versions/*/bin/
-    # XX will have to update this if we start supporting py2
-    PYTHON_EXE=/Library/Frameworks/Python.framework/Versions/*/bin/python3
+    if expr match "${MACPYTHON}" "3.*"; then
+        PYBASE=python3
+    else
+        PYBASE=python
+    fi
+    PYTHON_EXE=/Library/Frameworks/Python.framework/Versions/*/bin/${PYBASE}
     sudo $PYTHON_EXE -m pip install virtualenv
     $PYTHON_EXE -m virtualenv testenv
     source testenv/bin/activate
